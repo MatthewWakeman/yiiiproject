3517b69c998670a02bf4dbbeca8198144a0e7336
MatthewWakeman <57685047+MatthewWakeman@users.noreply.github.com>
2022-07-04 04:00:27 +0100

    Removed placeholder calculations

--- models/CalculateInteger.php
+++ models/CalculateInteger.php
@@ -14,14 +14,41 @@ class CalculateInteger extends Model {
     public function rules()
     {
         return [
-            [['value'], 'required']
+            [['value'], 'required'],
+            // [['value'], 'integer', 'max' <= 8],
         ];
     }
 
     public function calculate() {
         if ($this->validate()) {
-            $this->value += 1;
-            return $this->value;
+
+            $map = array('M' => 1000,
+                'CM' => 900,
+                'D' => 500,
+                'CD' => 400,
+                'C' => 100,
+                'XC' => 90,
+                'L' => 50,
+                'XL' => 40,
+                'X' => 10,
+                'IX' => 9,
+                'V' => 5,
+                'IV' => 4,
+                'I' => 1
+            );
+            $roman_value = '';
+            $number = $this->value;
+            while ($number > 0) {
+                foreach ($map as $roman => $int) {
+                    if($number >= $int) {
+                        $number -= $int;
+                        $roman_value .= $roman;
+                        break;
+                    }
+                }
+            }
+            return $roman_value;
+
         }
     }
 }
