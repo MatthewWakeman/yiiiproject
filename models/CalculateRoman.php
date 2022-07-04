960ad41b603898e6a0e1fe838698131826127edd
MatthewWakeman <57685047+MatthewWakeman@users.noreply.github.com>
2022-07-04 03:59:50 +0100

    Added removed placeholder calculations

--- models/CalculateRoman.php
+++ models/CalculateRoman.php
@@ -14,14 +14,37 @@ class CalculateRoman extends Model {
     public function rules()
     {
         return [
-            [['value'], 'required']
+            [['value'], 'required'],
+            // [['value'], 'string', 'max' <= 9]
         ];
     }
 
     public function calculate() {
         if ($this->validate()) {
-            $this->value += 1;
-            return $this->value;
+            
+            $roman_values = array(
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+            );
+            $roman = $this->value;
+            $integer = 0;
+            $prev = null;
+            for ($n = strlen($roman) - 1; $n >= 0; --$n) {
+                $curr = $roman[$n];
+                if ( is_null($prev) ) {
+                    $integer += $roman_values[$roman[$n]];
+                } else {
+                    $integer += $roman_values[$prev] > $roman_values[$curr] ? -$roman_values[$curr] : +$roman_values[$curr];
+                }
+                $prev = $curr;
+            }
+
+            return $integer;
         }
     }
 }
