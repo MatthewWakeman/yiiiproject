512fbdb4160e2cd476f83c014dfdd09cdbd490c1
MatthewWakeman <57685047+MatthewWakeman@users.noreply.github.com>
2022-07-04 02:57:04 +0100

    Added validation and a basic sum for now to test it works

---
+++ models/CalculateInteger.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class CalculateInteger extends Model {
+    public $value;
+
+    /**
+     * @return array the validation rules.
+     */
+    public function rules()
+    {
+        return [
+            [['value'], 'required']
+        ];
+    }
+
+    public function calculate() {
+        if ($this->validate()) {
+            $this->value += 1;
+            return $this->value;
+        }
+    }
+}
